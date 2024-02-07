b9044694c9a0dd418df92fd242b84cb91b17819f
Josh Elgar <joshelgar@gmail.com>
2024-02-07 22:39:43 +0000

    Asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+// next.config.js in the new project
+const assetPrefix = process.env.ASSET_PREFIX || "";
+
 const nextConfig = {
-  // assetPrefix: "/foodpixels",
+  assetPrefix,
 };
 
 module.exports = nextConfig;
