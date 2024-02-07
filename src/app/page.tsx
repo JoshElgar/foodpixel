616b6bf14d95ae018a8dfd6a9fa4ec8b9105c3f3
Josh Elgar <joshelgar@gmail.com>
2024-02-07 23:19:15 +0000

    Text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center py-8 lg:p-24">
       <RandomColorSquares />
-      <div className="text-sm mt-2">314,998 foods</div>
+      <div className="text-sm text-center mt-6 px-12">
+        1 pixel for every food in the USDA database (314,998)
+      </div>
     </main>
   );
 }
